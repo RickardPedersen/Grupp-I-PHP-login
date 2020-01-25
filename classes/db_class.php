ad214a1744f1d28980b294a29742fe86e490cd5c
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2020-01-25 12:03:07 +0100

    started on Register class

--- classes/db_class.php
+++ classes/db_class.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace classes;
+<?php namespace classes;
 
 class MySQL
 {
---
+++ classes/register_class.php
@@ -0,0 +1,15 @@
+<?php namespace classes;
+
+class Register
+{
+    private $username;
+    private $password;
+    private $email;
+
+    public function __construct($username, $password, $email)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
+}
