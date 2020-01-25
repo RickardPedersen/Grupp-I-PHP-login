c371592849639fafa23e655cedb49d8db8e130d9
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2020-01-25 19:47:02 +0100

    register class done

--- classes/register_class.php
+++ classes/register_class.php
@@ -3,15 +3,86 @@
 class Register
 {
     private $username;
-    private $password;
+    private $hashedPassword;
     private $email;
-    private $acceptedStatus;
 
     public function __construct($username, $password, $email)
     {
-        $this->username = $username;
-        $this->password = $password;
-        $this->email = $email;
-        $this->acceptedStatus = false;
+        $this->username = filter_var($username, FILTER_SANITIZE_STRING);
+        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $password = filter_var($password, FILTER_SANITIZE_STRING);
+
+        if (empty($password)) {
+            $this->hashedPassword = null;
+        } else {
+            $this->hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        }
+    }
+
+    public function validate()
+    {
+        // validate email
+        if (isset($this->email) == false ||
+            filter_var($this->email, FILTER_VALIDATE_EMAIL) == false ||
+            strlen($this->email) > 65
+        ) {
+            return false;
+        }
+
+        // validate username
+        if (isset($this->username) == false ||
+            strlen($this->username) > 20 ||
+            empty($this->username)
+        ) {
+            return false;
+        }
+
+        // validate password
+        if (isset($this->hashedPassword) == false ||
+            is_null($this->hashedPassword)
+        ) {
+            return false;
+        }
+
+        // all is validated
+        return true;
+    }
+
+    private function checkDuplicate($pdo)
+    {
+        $sql = "SELECT UserName, UserEmail
+                FROM users
+                WHERE UserName = :UserName
+                OR UserEmail = :UserEmail";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':UserName', $this->username);
+        $stmt->bindParam(':UserEmail', $this->email);
+        $stmt->execute();
+
+        if ($stmt->fetch() !== false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function addUser($pdo)
+    {
+        $accepted = $this->checkDuplicate($pdo);
+
+        if (!$accepted) {
+            return false;
+        }
+
+        $sql = "INSERT INTO users (UserName, UserPassword, UserEmail)
+                VALUES (:UserName, :UserPassword, :UserEmail)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':UserName', $this->username);
+        $stmt->bindParam(':UserPassword', $this->hashedPassword);
+        $stmt->bindParam(':UserEmail', $this->email);
+        $stmt->execute();
+        return true;
     }
 }
---
+++ views/register_test.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+$dotenv->load();
+
+$db = new classes\MySQL();
+$pdo = $db->connect();
+
+/*
+* Skapar ett user object
+* params (username, password, email)
+*/
+$user = new classes\Register('TestUser', '123', 'test@mail.com');
+
+// Filtrerar/validerar inputs
+if ($user->validate()) {
+    // inputs OK
+
+    // Försöker lägga till användare i databasen
+    if ($user->addUser($pdo)) {
+        // Success!
+        echo 'User created!';
+    } else {
+        // Användarnamn och/eller lösenord finns redan i databasen
+        echo 'Username or email taken';
+    }
+} else {
+    echo 'Illegal inputs';
+}
