3b54f42698f6bebf9ed5918d9b9e8106b62588b6
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2020-01-25 13:30:13 +0100

    stuff

--- classes/register_class.php
+++ classes/register_class.php
@@ -5,11 +5,13 @@ class Register
     private $username;
     private $password;
     private $email;
+    private $acceptedStatus;
 
     public function __construct($username, $password, $email)
     {
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
+        $this->acceptedStatus = false;
     }
 }
