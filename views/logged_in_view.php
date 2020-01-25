6b948351e6c0b994620ed7ca4e1dec98d37f9e79
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-01-25 17:09:47 +0100

    Removed form

--- views/logged_in_view.php
+++ views/logged_in_view.php
@@ -22,10 +22,8 @@ $loggedIn = new classes\LoggedInClass("Alex", "@testing");
                 <?php
                     echo $loggedIn->sendSessionData();
                 ?>
-             </h1>
-             <form action="logged_in_view.php" method="get">
-                 <button class="btn btn-lg btn-success" type="submit" name="button">Logout</button>
-             </form>
+            </h1>
+            <button class="btn btn-lg btn-success" type="submit" name="button">Logout</button>
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
