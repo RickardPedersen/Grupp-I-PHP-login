9115541a0b347dc0ff694053077adc2f5c46089c
esodermark <sodermark@live.se>
2020-01-26 19:23:14 +0100

    added test for loggedin, created folder tests, renamed test files to end with Test

---
+++ tests/LoggedInTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use classes\LoggedInClass;
+use PHPUnit\Framework\TestCase;
+
+class LoggedInUnitTesting extends TestCase
+{
+    protected $LoggedIn;
+
+    public function testSession()
+    {
+        $username = 'test';
+        $email = 'test@test.se';
+        $this->LoggedIn = new LoggedInClass($username, $email);
+        echo $this->LoggedIn->sendSessionData();
+        $this->assertEquals('test, test@test.se', $this->LoggedIn->sendSessionData());
+    }
+}
