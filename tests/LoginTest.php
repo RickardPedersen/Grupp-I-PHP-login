5e731ed9e39b5bf168c57804320870f9ea6a68c9
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-01-27 16:19:26 +0100

    Made a skeleton for LoginUnitTesting.

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,6 +1,14 @@
 <?php
 
-class LoginUnitTesting
-{
+require __DIR__ . '/../vendor/autoload.php';
 
+use classes\LoggedInClass;
+use PHPUnit\Framework\TestCase;
+
+class LoginUnitTesting extends TestCase
+{
+    public function testLogin()
+    {
+
+    }
 }
