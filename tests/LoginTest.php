9e9b05bd9d3a19900efcbddfbb618fc032826921
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-01-27 20:53:10 +0100

    Added testing for login username

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -7,8 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class LoginUnitTesting extends TestCase
 {
-    public function testLogin()
-    {
 
+    public function testLoginIsObject()
+    {
+        //Assert that login __construct returns an object.
+        $usernameOrEmail = "TestUser";
+        $password = "123";
+        $this->assertIsObject(new classes\Login($usernameOrEmail, $password));
+    }
+    public function testUsernameIsString()
+    {
+        $username = 123;
+        $filteredNumber = filter_var($username, FILTER_SANITIZE_STRING);
+        $this->assertIsString($filteredNumber);
+        $username = null;
+        $filteredNull = filter_var($username, FILTER_SANITIZE_STRING);
+        $this->assertIsString($filteredNull);
     }
 }
