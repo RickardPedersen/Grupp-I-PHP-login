f6285527bba5812374073ac325393c9e915ca53b
esodermark <sodermark@live.se>
2020-01-30 18:32:22 +0100

    loggedin test

--- tests/LoggedInTest.php
+++ tests/LoggedInTest.php
@@ -5,18 +5,28 @@ namespace tests;
 require __DIR__ . '/../vendor/autoload.php';
 
 use classes\LoggedInClass;
+use classes\Login;
+use classes\MySQL;
 use PHPUnit\Framework\TestCase;
 
 class LoggedInUnitTesting extends TestCase
 {
     protected $LoggedIn;
+    protected $Login;
 
     public function testSession()
     {
-        $username = 'test';
-        $email = 'test@test.se';
-        $this->LoggedIn = new LoggedInClass($username, $email);
-        echo $this->LoggedIn->sendSessionData();
-        $this->assertEquals('test, test@test.se', $this->LoggedIn->sendSessionData());
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+        $dotenv->load();
+        $db = new MySQL();
+        $pdo = $db->connect();
+
+        $username = 'nejnej';
+        $password = 'nejnej';
+
+        $this->Login = new Login($username, $password);
+        $this->Login->login($pdo);
+        $this->LoggedIn = new LoggedInClass();
+        $this->assertEquals(array('username'=>'nejnej', 'email'=>'nej@nej.se'), $this->LoggedIn->sendSessionData());
     }
 }
